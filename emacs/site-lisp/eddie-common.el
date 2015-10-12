ebae1f2a4eed0f546abe5f16d39f8c71ec576f32
Eddie McCreary <software@heorot.org>
2015-10-11 20:03:25 -0500

    update, company

--- emacs/site-lisp/eddie-common.el
+++ emacs/site-lisp/eddie-common.el
@@ -1,41 +1,54 @@
-;; eddie-common.el
-
-;; add this to 
-;; (add-to-list 'load-path "~/.emacs.d")
-;; (require 'eddie-common)
-;; mkdir ~/.saves
-;; TODO
-;;(add-to-list 'load-path "/usr/local/go/misc/emacs")
-;;(require 'go-mode-load)
 (provide 'eddie-common)
 
-(load "ggtags")
 
+;;(require 'xcscope)
+;;(setq cscope-do-not-update-database t)
+;;(setq cscope-initial-directory "~/bin")
+(require 'xgtags)
 (add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              (ggtags-mode 1))))
+		  (lambda ()
+			(c-set-offset 'case-label '+)
+			(xgtags-mode 1)))
 
+(defun gtags-root-dir ()
+  "Returns GTAGS root directory or nil if doesn't exist."
+  (with-temp-buffer
+	(if (zerop (call-process "global" nil t nil "-pr"))
+		(buffer-substring (point-min) (1- (point-max)))
+	  nil)))
+
+(defun gtags-update-single(filename)  
+  "Update Gtags database for changes in a single file"
+  (interactive)
+  (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --single-update " filename )))
+
+(defun gtags-update-current-file()
+  (interactive)
+  (defvar filename)
+  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
+  (gtags-update-single filename)
+  (message "Gtags updated for %s" filename))
+
+(defun gtags-update-hook()
+  "Update GTAGS file incrementally upon saving a file"
+  (when xgtags-mode
+	(when (gtags-root-dir)
+	  (gtags-update-current-file))))
+
+(add-hook 'after-save-hook 'gtags-update-hook)
+
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(show-paren-mode 1)
+
+(require 'go-mode-load)
 ;; turn on font-lock mode
 (when (fboundp 'global-font-lock-mode)
   (global-font-lock-mode t))
 
 ;; enable visual feedback on selections
 (setq transient-mark-mode t)
-
-;; left side line number
-(global-linum-mode t)
-
-;; save open files and place in file
-(custom-set-variables
-  '(desktop-enable t nil (desktop))
-  '(save-place t nil (saveplace)))
-
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ;; default to better frame titles
 (setq frame-title-format
@@ -49,23 +62,17 @@
 
 (global-set-key [f5] 'compile)
 (global-set-key [f6] 'find-grep-dired)
+(global-set-key [f4] 'svn-status)
 (global-set-key [f2] 'delete-trailing-whitespace)
 
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
+(setq c-default-style "k&r"
+	            c-basic-offset 4)
 (line-number-mode t)    ; makes the line number show up
 (column-number-mode t)  ; makes the column number show up
-(tool-bar-mode nil)
 
-(setq c-default-style "k&r"
-          c-basic-offset 4)
-
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode t)
-	    (setq tab-width 2)))
-
-;;(desktop-save-mode t)
+(desktop-save-mode t)
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
@@ -74,7 +81,6 @@
           kept-old-versions 2
           version-control t)
 
-(require 'psvn)
 (defun eddie-timestamp ()
   "Spit out the current time"
   (interactive)
@@ -83,7 +89,7 @@
 (defun eddie-sign ()
   "spit out my name, email and the current time"
   (interactive)
-  (insert "-- Eddie McCreary <software@heorot.com>")
+  (insert "-- Eddie McCreary <eddie.mccreary@hp.com>")
   (eddie-timestamp))
 
 (defun save-framegeometry ()
@@ -109,6 +115,7 @@
       (when (file-writable-p framegeometry-file)
         (write-file framegeometry-file))))
   )
+
 (defun load-framegeometry ()
   "Loads ~/.emacs.d/framegeometry which should load the previous frame's geometry."
   (let ((framegeometry-file (expand-file-name "~/.emacs.d/framegeometry")))
@@ -123,11 +130,34 @@
 ;;      (add-hook 'kill-emacs-hook 'save-framegeometry))
 ;;  )
 
+;; eof
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+;;(load-file "~/emacs/cedet/common/cedet.el")
+
+;;(add-to-list 'load-path "~/emacs/ecb")
+;;(add-to-list 'load-path "~/emacs/color-theme")
 (add-to-list 'load-path "~/.emacs.d/themes")
 
-''(require 'color-theme)
+;;(require 'color-theme)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;;(require 'color-theme-solarized)
+;;(load-theme 'solarized-dark t)
 (load-theme 'github t)
+;;(require 'ecb-autoloads)
+;;(setq ecb-tip-of-the-day nil)
+
+;; Enable EDE (Project Management) features
+;;(global-ede-mode 1)
+
+;; Enabling Semantic (code-parsing, smart completion) features
+;; Select one of the following:
+
+;; * This enables the database and idle reparse engines
+;;(semantic-load-enable-minimum-features)
+;; * This enables some tools useful for coding, such as summary mode
+;;   imenu support, and the semantic navigator
+;;(semantic-load-enable-code-helpers)
 
 (defun my-compilation-hook ()
   (when (not (get-buffer-window "*compilation*"))
@@ -140,3 +170,19 @@
           (shrink-window (- h 10)))))))
 (add-hook 'compilation-mode-hook 'my-compilation-hook)
 
+(tool-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 90 :width normal)))))

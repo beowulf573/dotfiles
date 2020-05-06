3f7ce6bd1886ce378cd35869ca2b0519ef23e0a5
Eddie McCreary <eddie.mccreary@heorot.org>
2020-05-06 13:10:59 -0500

    cleanup, ivy

--- emacs/site-lisp/eddie-common.el
+++ emacs/site-lisp/eddie-common.el
@@ -26,27 +26,13 @@ There are two things you can do about this warning:
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
-;; (require 'mk-project)
-
-;; (global-set-key (kbd "C-c p c") 'project-compile)
-;; (global-set-key (kbd "C-c p l") 'project-load)
-;; (global-set-key (kbd "C-c p a") 'project-ack)
-;; (global-set-key (kbd "C-c p g") 'project-grep)
-;; (global-set-key (kbd "C-c p o") 'project-multi-occur)
-;; (global-set-key (kbd "C-c p u") 'project-unload)
-;; (global-set-key (kbd "C-c p f") 'project-find-file) ; or project-find-file-ido
-;; (global-set-key (kbd "C-c p i") 'project-index)
-;; (global-set-key (kbd "C-c p s") 'project-status)
-;; (global-set-key (kbd "C-c p h") 'project-home)
-;; (global-set-key (kbd "C-c p d") 'project-dired)
-;; (global-set-key (kbd "C-c p t") 'project-tags)
-
 ;; add project directory in common
 ;; (setq projectile-project-search-path '("~/projects/"e))
 (require 'projectile)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
+(setq projectile-completion-system 'ivy)
 
 (load-theme 'nord t)
 (set-face-attribute 'font-lock-comment-face nil
@@ -54,13 +40,9 @@ There are two things you can do about this warning:
 (set-face-attribute 'vertical-border nil
 	                      :foreground "#EBCB8B") ; nord13
 
-;; (ac-config-default)
 
 (require 'xcscope)
 (cscope-setup)
-;;(setq cscope-do-not-update-database t)
-;;(setq cscope-initial-directory "~/bin")
-;(require 'ggtags)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (custom-set-variables
@@ -69,38 +51,6 @@ There are two things you can do about this warning:
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))))
-
-;; (add-hook 'c-mode-common-hook
-;; 		  (lambda ()
-;; 			(c-set-offset 'case-label '+)
-;; 			(ggtags-mode 1)))
-
-;; (defun gtags-root-dir ()
-;;   "Returns GTAGS root directory or nil if doesn't exist."
-;;   (with-temp-buffer
-;; 	(if (zerop (call-process "global" nil t nil "-pr"))
-;; 		(buffer-substring (point-min) (1- (point-max)))
-;; 	  nil)))
-
-;; (defun gtags-update-single(filename)
-;;   "Update Gtags database for changes in a single file"
-;;   (interactive)
-;;   (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --single-update " filename )))
-
-;; (defun gtags-update-current-file()
-;;   (interactive)
-;;   (defvar filename)
-;;   (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
-;;   (gtags-update-single filename)
-;;   (message "Gtags updated for %s" filename))
-
-;; (defun gtags-update-hook()
-;;   "Update GTAGS file incrementally upon saving a file"
-;;   (when ggtags-mode
-;; 	(when (gtags-root-dir)
-;; 	  (gtags-update-current-file))))
-
-;; (add-hook 'after-save-hook 'gtags-update-hook)
 
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
@@ -190,41 +140,7 @@ There are two things you can do about this warning:
       (load-file framegeometry-file)))
   )
 
-;; Special work to do ONLY when there is a window system being used
-;;(if window-system
-;;    (progn
-;;      (add-hook 'after-init-hook 'load-framegeometry)
-;;      (add-hook 'kill-emacs-hook 'save-framegeometry))
-;;  )
-
-;; eof
-;; Load CEDET.
-;; See cedet/common/cedet.info for configuration details.
-;;(load-file "~/emacs/cedet/common/cedet.el")
-
-;;(add-to-list 'load-path "~/emacs/ecb")
-;;(add-to-list 'load-path "~/emacs/color-theme")
-(add-to-list 'load-path "~/.emacs.d/themes")
-
-;;(require 'color-theme)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;;(require 'color-theme-solarized)
-;;(load-theme 'solarized-dark t)
-;;(load-theme 'github t)
-;;(require 'ecb-autoloads)
-;;(setq ecb-tip-of-the-day nil)
-
-;; Enable EDE (Project Management) features
-;;(global-ede-mode 1)
-
-;; Enabling Semantic (code-parsing, smart completion) features
-;; Select one of the following:
-
-;; * This enables the database and idle reparse engines
-;;(semantic-load-enable-minimum-features)
-;; * This enables some tools useful for coding, such as summary mode
-;;   imenu support, and the semantic navigator
-;;(semantic-load-enable-code-helpers)
+;; (add-to-list 'load-path "~/.emacs.d/themes")
 
 (defun my-compilation-hook ()
   (when (not (get-buffer-window "*compilation*"))

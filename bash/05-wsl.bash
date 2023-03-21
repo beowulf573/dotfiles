8e7d8f9a680047b993a635bdab4a884e8e6915e6
Eddie McCreary <eddie.mccreary@hpe.com>
2023-03-21 09:03:03 -0500

    redirect

--- bash/05-wsl.bash
+++ bash/05-wsl.bash
@@ -1,5 +1,5 @@
 # work around odd bug in wsl, would get Invalid Arguments otherwise
-pushd /mnt/c
+pushd /mnt/c > /dev/null
 export WSL_VERSION=$(wsl.exe -l -v | grep -a '[*]' | sed 's/[^0-9]*//g')
 if [ "$WSL_VERSION" -eq "2" ] ; then
 	export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
@@ -7,4 +7,4 @@ if [ "$WSL_VERSION" -eq "2" ] ; then
 else
 	export DISPLAY=127.0.0.1:0.0
 fi
-popd
+popd > /dev/null

d2106a8516fcdeda195e0d0673b9239526104e2f
Eddie McCreary <eddie.mccreary@hpe.com>
2023-03-21 09:01:23 -0500

    wsl push/pop

--- bash/05-wsl.bash
+++ bash/05-wsl.bash
@@ -1,4 +1,5 @@
-
+# work around odd bug in wsl, would get Invalid Arguments otherwise
+pushd /mnt/c
 export WSL_VERSION=$(wsl.exe -l -v | grep -a '[*]' | sed 's/[^0-9]*//g')
 if [ "$WSL_VERSION" -eq "2" ] ; then
 	export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
@@ -6,4 +7,4 @@ if [ "$WSL_VERSION" -eq "2" ] ; then
 else
 	export DISPLAY=127.0.0.1:0.0
 fi
-
+popd
